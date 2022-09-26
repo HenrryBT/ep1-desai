3bc4f7929ac058b974f1a2db496b8b13d5d3eb14
Henrry Beltran <henrrybt@gmail.com>
2022-09-25 20:20:28 -0500

    Setting up the proyect

---
+++ src/main/java/com/henrrybeltran/evaluacionpermanente1/Evaluacionpermanente1Application.java
@@ -0,0 +1,13 @@
+package com.henrrybeltran.evaluacionpermanente1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacionpermanente1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacionpermanente1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrrybeltran/evaluacionpermanente1/Evaluacionpermanente1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrrybeltran.evaluacionpermanente1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacionpermanente1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
